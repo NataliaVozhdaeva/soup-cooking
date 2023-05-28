d86872709c8ce608584e2dc25d765879f3ce1d0d
NataliaVozhdaeva <LomzNat@yandex.ru>
2023-05-28 10:33:35 +0700

    feat: finish first step

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-let bouillonBtn = document.querySelector('.hidden-span');
+const bouillonSection = document.querySelector('.bouillon');
+const bouillonList = document.querySelector('.bouillon-list');
 
-bouillonBtn.addEventListener('animationend', () => {
-  console.log('end ', bouillonBtn);
+const createBoulionEl = (el) => {
+  const bouillonPic = document.createElement('img');
+  bouillonPic.setAttribute('src', `./assets/content/${el.dataset.bouillon}.png`);
+  bouillonPic.className = 'bouillon-pic';
+  bouillonSection.append(bouillonPic);
+};
+
+bouillonList.addEventListener('click', (e) => {
+  console.log(e.target.parentNode);
+  createBoulionEl(e.target.parentNode);
+  bouillonList.classList.add('non-displayed');
 });
