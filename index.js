f13454d5d58c06d7cdbeea1f1d4f7f7663a1ea9d
NataliaVozhdaeva <LomzNat@yandex.ru>
2023-05-28 15:27:07 +0700

    feat: add menu

--- index.js
+++ index.js
@@ -1,15 +1,76 @@
 const bouillonSection = document.querySelector('.bouillon');
+const menuSection = document.querySelector('.menu');
+const menuBtns = document.querySelector('.menu-btns');
+const ingredientsSection = document.querySelector('.ingredients');
+const processSection = document.querySelector('.process');
+
 const bouillonList = document.querySelector('.bouillon-list');
 
-const createBoulionEl = (el) => {
+const menu = {
+  meat: ['Щи', 'Борщ', 'Чечевичный', 'Грибной', 'С клецками', 'Самое простое/быстрое/ващенекогда'],
+  fish: ['С консервой'],
+  chiken: ['С вермишелью', 'С горошком'],
+};
+const bouillon = ['мясо', 'рыба', 'курица'];
+const frying = ['лук', 'морковка'];
+const fryingOptional = ['чеснок', 'свекла', 'помидор', 'перец болгарский'];
+const mainIngredients = [
+  'картошка',
+  'капуста',
+  'горошек замороженный',
+  'рис',
+  'вермишель',
+  'чечевица',
+  'грибы',
+  'клецки',
+];
+
+const addArrow = () => {
+  const arrowSpan = document.createElement('div');
+  arrowSpan.className = 'arrow';
+  bouillonSection.append(arrowSpan);
+};
+
+const createBoulionEl = (data) => {
   const bouillonPic = document.createElement('img');
-  bouillonPic.setAttribute('src', `./assets/content/${el.dataset.bouillon}.png`);
+  bouillonPic.setAttribute('src', `./assets/content/${data}.png`);
   bouillonPic.className = 'bouillon-pic';
   bouillonSection.append(bouillonPic);
 };
 
+const createMenuBtn = (item) => {
+  const menuItem = document.createElement('button');
+  menuItem.className = 'btn, menu-btn';
+  menuItem.setAttribute('data-item', item);
+  menuItem.textContent = item;
+
+  menuBtns.append(menuItem);
+};
+
+const createMenuSection = (data) => {
+  const menuTitle = document.createElement('h3');
+  menuTitle.textContent = 'Какой суп?';
+  menuSection.prepend(menuTitle);
+  for (let i = 0; i < menu[data].length; i++)
+    setTimeout(() => {
+      createMenuBtn(menu[data][i]);
+    }, i * 300);
+};
+
 bouillonList.addEventListener('click', (e) => {
-  console.log(e.target.parentNode);
-  createBoulionEl(e.target.parentNode);
+  const data = e.target.parentNode.dataset.bouillon;
+  createBoulionEl(data);
   bouillonList.classList.add('non-displayed');
+  document.querySelector('.bouillon-title').classList.add('non-displayed');
+  createMenuSection(data);
+});
+
+menuBtns.addEventListener('click', (e) => {
+  console.log(e.target.dataset.item);
+  addArrow();
+  const item = document.createElement('button');
+  item.className = 'btn, header-btn';
+  item.textContent = e.target.dataset.item;
+  bouillonSection.append(item);
+  menuSection.classList.add('non-displayed');
 });
